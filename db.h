7a92b2f98cf0d5fa4aaf5b353c4bda84bdbdd133
Peerchemist <dx.1178@gmail.com>
2016-10-30 12:17:44 +0100

    require latest peercoin version.

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 60004
+#define REQUIRE_VERSION 60006
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 163997;
+    return testnet ? 0 : 200000;
 }
 
 std::string static inline ToString(const CService &ip) {
